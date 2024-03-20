51bfdca0959dcbef031c2e83b8b4f9c01a678c01
Dmitry <boetz@yandex.ru>
2024-03-20 23:07:36 +0700

    Added SQLLite

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -6,8 +6,10 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositorySQLiteImpl
 
 private val empty = Post(
     id = 0,
@@ -19,7 +21,12 @@ private val empty = Post(
 
 class PostViewModel (application: Application) : AndroidViewModel(application) {
     // упрощённый вариант
-    private val repository: PostRepository = PostRepositoryFileImpl(application)
+    /*private val repository: PostRepository = PostRepositoryFileImpl(application)*/
+
+    private val repository: PostRepository = PostRepositorySQLiteImpl(
+        AppDb.getInstance(application).postDao
+    )
+
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
     fun save() {
