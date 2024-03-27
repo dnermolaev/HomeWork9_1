7bfee011b0ec9418feaaaaf1586742ecb8a95850
Dmitry <boetz@yandex.ru>
2024-03-27 23:58:47 +0700

    Room added

---
+++ app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
@@ -0,0 +1,26 @@
+package ru.netology.nmedia.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.netology.nmedia.dto.Post
+
+@Entity
+data class PostEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    val shares: Int = 0,
+    val videoLink: String? = null
+) {
+    fun toDto() = Post(id, author, content, published,  likes, likedByMe, shares, videoLink)
+
+    companion object {
+        fun fromDto(dto: Post) =
+            PostEntity(dto.id, dto.author, dto.content, dto.published, dto.likedByMe, dto.likes, dto.shares, dto.videoLink)
+
+    }
+}
